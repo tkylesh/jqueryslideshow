dcbb491158e2d99f6c999800184b812be8b91b13
Taylor Shuff <tkylesh@gmail.com>
2016-10-03 20:30:31 -0500

    added nasa developer api key because demo key didn't work

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,4 +1,4 @@
-$.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY",
+$.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK",
 	function(data) {
 		console.log(data);
 		$("#slideshow").html(data);
