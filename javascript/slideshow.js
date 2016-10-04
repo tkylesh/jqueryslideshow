025c94f1136226b2c1876ab6175df4652a348cc5
Taylor Shuff <tkylesh@gmail.com>
2016-10-03 20:53:16 -0500

    successfully made ajax call using jquery to nasa daily photo api. displays the object in the form of a string on the web page.

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,6 +1,6 @@
 $.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK",
 	function(data) {
 		console.log(data);
-		$("#slideshow").html(data);
+		$("#slideshow").html(JSON.stringify(data));
 		console.log("Load was performed.");
 	});
