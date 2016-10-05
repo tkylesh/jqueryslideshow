b85dedbcf12ac5629efd65fc9f5e292a601aa4e0
Taylor Shuff <tkylesh@gmail.com>
2016-10-05 13:23:14 -0500

    corrected some issues with the javascript promises.  Now the asynchronous calls are executing and pushing to the array in correct order.  Each title and hdurl is displayed in console.

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,7 +1,7 @@
 
 var imageArray = [];
-
 var dateArray = [];
+var $slideshow = $('#slideshow');
 //get the current date
 //then get the image from that day
 //and the three previous to use in slideshow
@@ -23,16 +23,12 @@ for(var i =0; i < 4; i++){
 console.log(dateArray);
 
 
-
 //Four jquery get requests that return one json object each
-
 var firstAJAX = function() {
 	return new Promise((resolve, reject)=> {
-		$.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK&date="+dateArray[0],
-		function(data) {
-			imageArray.push(data);
-		}).done(function(){
-			resolve();
+		$.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK&date="+dateArray[0])
+		.done(function(data){
+			resolve(imageArray.push(data));
 		}).fail(function(xhr, status, error){
 			reject(error);
 		});
@@ -40,11 +36,9 @@ var firstAJAX = function() {
 };
 var secondAJAX = function() {
 	return new Promise((resolve, reject)=> {
-		$.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK&date="+dateArray[1],
-		function(data) {
-		imageArray.push(data);
-		}).done(function(){
-			resolve();
+		$.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK&date="+dateArray[1])
+		.done(function(data){
+			resolve(imageArray.push(data));
 		}).fail(function(xhr, status, error){
 			reject(error);
 		});
@@ -52,11 +46,9 @@ var secondAJAX = function() {
 };
 var thirdAJAX = function() {
 	return new Promise((resolve, reject)=> {
-		$.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK&date="+dateArray[2],
-		function(data) {
-			imageArray.push(data);
-		}).done(function(){
-			resolve();
+		$.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK&date="+dateArray[2])
+		.done(function(data){
+			resolve(imageArray.push(data));
 		}).fail(function(xhr, status, error){
 			reject(error);
 		});
@@ -64,59 +56,35 @@ var thirdAJAX = function() {
 };
 var fourthAJAX = function() {
 	return new Promise((resolve, reject)=> {
-		$.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK&date="+dateArray[3],
-		function(data) {
-			console.log("data3", data)
-			imageArray.push(data);
-		}).done(function(){
-			console.log("imageArray", imageArray)
-			resolve();
+		$.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK&date="+dateArray[3])
+		.done(function(data){
+			resolve(imageArray.push(data));
 		}).fail(function(xhr, status, error){
 			reject(error);
 		});
 	})
 };
-// $.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK&date="+dateArray[1],
-// 	function(data) {
-// 		imageArray.push(data);
-// 	});
-// $.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK&date="+dateArray[2],
-// 	function(data) {
-// 		imageArray.push(data);
-// 	});
-// $.get("https://api.nasa.gov/planetary/apod?api_key=cCEW3xxeVKbq40XJmNlblQG0BBQSBQ5FUQcqJuPK&date="+dateArray[3],
-// 	function(data) {
-// 		imageArray.push(data);
-// 	});
+
+
+
+
 //goals:
 //push all of the images into an array then populate the page using the array
-firstAJAX()
-	.then(function(){
-		console.log("date1", Date.now())
-		secondAJAX();
+(function(){
+	firstAJAX()
+	.then(secondAJAX)
+	.then(thirdAJAX)
+	.then(fourthAJAX)
+	.then(
+		function(){
+		console.log("imageArray", imageArray)
+		console.log(imageArray.length)
+		for(var i =0; i < imageArray.length; i++){
+			console.log("title: ",imageArray[i].title," | ","hdurl: ", imageArray[i].hdurl);
+		};
 	})
-	.then(function(){
-		console.log("date2", Date.now())
-		thirdAJAX();
-	})
-	.then(function(){
-		console.log("date3", Date.now())
-		fourthAJAX();
-	})
-	.then(function(){
-		// console.log("date4", Date.now())
-		// console.log(imageArray);
-		// console.log(imageArray.length);
-		// console.log(imageArray[2]);
+})();
 
-
-		// for(var i =0; i < imageArray.length; i++){
-		// 	console.log(imageArray[i]);
-		// };
-
-		// $.each(imageArray,function(){
-		// 	$.each(this, function (name, value) {
-		//     console.log(name + '=' + value);
-		//    })
-		// });
-	});
+//when dom is ready to be manipulated
+$(document).ready(function() { 
+});
